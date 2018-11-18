af2765cae3dedb585cdfcbc5ace3027f404b98f3
daniellemachpud <daniellemachpud@danielles-MacBook-Pro.local>
2018-11-18 15:56:25 +0200

    add python project that decrypt file

---
+++ decryptFile.py
@@ -0,0 +1,22 @@
+import pyAesCrypt
+import gitbxlogger
+
+#   create get tag logger
+dec_file_logger = gitbxlogger.myLooger('decFileLogger')
+#   Length of file to decrypt
+FILE_SIZE = 64*1024
+#   Public key
+PASSWORD = "versionbx"
+
+
+def decrypt_file(src_dec_file, dst_dec_file):
+    data = ""
+    # decrypt
+    try:
+        pyAesCrypt.decryptFile(src_dec_file, dst_dec_file, PASSWORD, FILE_SIZE)
+        dec_file_data = open(dst_dec_file)
+        data = dec_file_data.read()
+    except Exception as ex:
+        dec_file_logger.logger.fatal(ex)
+
+    return data
